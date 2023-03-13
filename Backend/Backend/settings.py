accbbef92147a8064522801b51c99e3ba2b063ab
idodd <idoddii@gmail.com>
2023-03-13 12:37:48 +0200

    Connected the Database

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -78,10 +78,15 @@ WSGI_APPLICATION = 'Backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'plant_it_app',
+        'USER': 'root',
+        'PASSWORD': 'SantIdOfek',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
