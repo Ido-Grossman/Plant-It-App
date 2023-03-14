f58b6602a23c0dd50b8b8f74dc7e7f7dd584fda2
idodd <idoddii@gmail.com>
2023-03-14 19:32:59 +0200

    Registration and login works

--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from Backend import PlantItApp
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(PlantItApp.urls))
+    path('api/', include('PlantItApp.urls'))
 ]
--- Backend/PlantItApp/controllers/RegisterController.py
+++ Backend/PlantItApp/controllers/RegisterController.py
@@ -1,7 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
 from rest_framework import status
 
 
@@ -12,15 +11,3 @@ def Register(request):
     email = request.data['email']
     user = User.objects.create_user(user_name, email, password)
     return Response(status=status.HTTP_201_CREATED)
-
-
-@api_view(['Post'])
-def Login(request):
-    user_name = request.data['username']
-    password = request.data['password']
-    user = authenticate(request, username=user_name, password=password)
-    if user is not None:
-        login(request, user)
-        return Response(status=status.HTTP_200_OK)
-    else:
-        return Response(status=status.HTTP_404_NOT_FOUND)
