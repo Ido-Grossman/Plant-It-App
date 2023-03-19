91db1be44a27f8a1dfbe59e580e9fda5dd321129
Santiago Szterenberg <santisz7897@gmail.com>
2023-03-19 18:43:24 +0200

    maybe fix http request

--- Frontend/lib/service/httpService.dart
+++ Frontend/lib/service/httpService.dart
@@ -7,7 +7,8 @@ class HttpService {
   Future<String> uploadPhoto(String path) async {
     Uri uri = Uri.parse('http://10.0.2.2:8000/api/photo-upload');
     http.MultipartRequest request = http.MultipartRequest('POST', uri);
-    request.files.add(await http.MultipartFile.fromPath('files', path));
+    http.MultipartFile multipartFile = await http.MultipartFile.fromPath('file', path);
+    request.files.add(multipartFile);
 
     http.StreamedResponse response = await request.send();
     var responseBytes = await response.stream.toBytes();
