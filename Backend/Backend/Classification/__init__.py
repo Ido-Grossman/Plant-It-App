39a8999442ba15668dc420020ef7c78cb747bfdf
idodd <idoddii@gmail.com>
2023-03-20 12:15:14 +0200

    Disease classification is complete

--- Backend/Backend/Classification/__init__.py
+++ Backend/Backend/Classification/__init__.py
@@ -1,1 +1,2 @@
 from . import ModelClasses
+from . import Prediction_Classes
--- Backend/PlantItApp/controllers/PhotoController.py
+++ Backend/PlantItApp/controllers/PhotoController.py
@@ -1,23 +1,10 @@
-import base64
-import os
 from PIL import Image
-
-from django.core.files.base import ContentFile
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from torchvision.datasets import ImageFolder  # for working with classes and images
 import torchvision.transforms as transforms  # for transforming images into tensors
 from django.conf import settings
 import torch
-
-from ..serializers import *
-
-
-def save_base64_as_image(data, filename):
-    format, imgstr = data.split(';base64,')
-    ext = format.split('/')[-1]
-    data = ContentFile(base64.b64decode(imgstr), name=filename + '.' + ext)
-    return data
+import os
 
 
 class PhotoUploadView(APIView):
@@ -28,12 +15,10 @@ class PhotoUploadView(APIView):
                 # Assuming you want to save the image in a 'media' folder
                 img = Image.open(image_file)
                 img = img.resize((256, 256))
-                img = img.convert('RGB')
-                save_path = os.path.join('media\\media', image_file.name)
-                img.save(save_path)
-        test_pic = 'C:\\Users\\idodd\\PycharmProjects\\Plant-It-App\\Backend\\media'
-        test = ImageFolder(test_pic, transform=transforms.ToTensor())
-        img, label = test[0]
-        yb = settings.MODEL(img.unsqueeze(0))
-        _, preds = torch.max(yb, dim=1)
-        return Response(int(preds))
+                img = img.rotate(270)
+                img.show()
+                transform = transforms.ToTensor()
+                img = transform(img)
+                yb = settings.MODEL(torch.unsqueeze(img, 0))
+                _, preds = torch.max(yb, dim=1)
+                return Response(settings.DISEASES[preds[0].item()])
