f4beeca94a0fa9c3b2bf8c08c0bb531dd808ee4b
idodd <idoddii@gmail.com>
2023-03-21 13:17:09 +0200

    Migrations is done

--- Backend/PlantItApp/models.py
+++ Backend/PlantItApp/models.py
@@ -1,16 +1,18 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
 from django.contrib.auth.models import User
 
 
 class Plant(models.Model):
     name = models.CharField(max_length=150)
     water_duration = models.IntegerField()
-    is_healthy = models.SmallIntegerField()
-    optimal_weather = models.CharField(max_length=150)
-    water_amount = models.IntegerField(0)
+    water_consumption = models.CharField(default=None, max_length=300)
+    is_healthy = models.SmallIntegerField(default=1)
+    minimum_celsius = models.IntegerField(default=0)
+    maximum_celsius = models.IntegerField(default=20)
+    sun_light = models.CharField(max_length=100, default=None)
+    humidity = models.IntegerField(default=None)
+    type = models.CharField(max_length=100, default=None)
+    description = models.CharField(max_length=1000, default=None)
     users = models.ManyToManyField(User, through='User_Plants')
 
 
