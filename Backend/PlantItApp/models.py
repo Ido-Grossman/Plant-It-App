03989ecdfa65ac975740e9f357b42d25b7e425f1
idodd <idoddii@gmail.com>
2023-03-24 17:08:31 +0300

    Updated the db tables

--- Backend/PlantItApp/models.py
+++ Backend/PlantItApp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Plant(models.Model):
+class Plant_Genus(models.Model):
     name = models.CharField(max_length=150)
     water_duration = models.IntegerField()
     water_consumption = models.CharField(default=None, max_length=300)
@@ -13,10 +13,15 @@ class Plant(models.Model):
     humidity = models.IntegerField(default=None)
     type = models.CharField(max_length=100, default=None)
     description = models.CharField(max_length=1000, default=None)
+
+
+class Plant(models.Model):
+    name = models.CharField(max_length=150)
+    genus = models.ForeignKey(Plant_Genus, default=None, on_delete=models.CASCADE)
     users = models.ManyToManyField(User, through='User_Plants')
 
 
 class User_Plants(models.Model):
-    plant_id = models.ForeignKey(Plant, on_delete=models.CASCADE)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    plant = models.ForeignKey(Plant, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     last_watering = models.DateField(6)
