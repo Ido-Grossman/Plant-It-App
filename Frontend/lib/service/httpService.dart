535f952426c4fca62df68935fa5bfa0ecd39c14b
Santiago Szterenberg <santisz7897@gmail.com>
2023-04-07 16:27:36 +0300

    change camera ui screen

--- Frontend/lib/service/httpService.dart
+++ Frontend/lib/service/httpService.dart
@@ -1,11 +1,12 @@
 import 'dart:convert';
 
+import 'package:frontend/constants.dart';
 import 'package:http/http.dart' as http;
 
 class HttpService {
   
   Future<String> uploadPhoto(String path) async {
-    Uri uri = Uri.parse('http://10.0.2.2:8000/api/photo-upload/');
+    Uri uri = Uri.parse('${Consts.prefixLink}/api/photo-upload/');
     http.MultipartRequest request = http.MultipartRequest('POST', uri);
     http.MultipartFile multipartFile = await http.MultipartFile.fromPath('file', path);
     request.files.add(multipartFile);
@@ -13,10 +14,7 @@ class HttpService {
     http.StreamedResponse response = await request.send();
     var responseBytes = await response.stream.toBytes();
     var responseString = utf8.decode(responseBytes);
-    print('\n\n');
-    print('RESPONSE WITH HTTP');
     print(responseString);
-    print('\n\n');
     return responseString;
   }
 
