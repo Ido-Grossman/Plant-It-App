98aaf224fcc80fda7a2bfff02e9086f226aa8861
Santiago Szterenberg <santiago.szterenberg@weka.io>
2023-04-11 15:45:10 +0300

    add authorization for token for google and normal login

--- Frontend/lib/logins/choosePassScreen.dart
+++ Frontend/lib/logins/choosePassScreen.dart
@@ -30,12 +30,12 @@ class _ChoosePassScreenState extends State<ChoosePassScreen> with TickerProvider
   @override
   void initState() {
     super.initState();
-    _gifController = GifController(vsync: this); // Add this line
+    _gifController = GifController(vsync: this);
   }
 
   @override
   void dispose() {
-    _gifController.dispose(); // Add this line
+    _gifController.dispose();
     super.dispose();
   }
 
