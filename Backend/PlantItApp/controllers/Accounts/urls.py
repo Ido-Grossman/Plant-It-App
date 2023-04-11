2a1fb414c8ef61a1dd9e49c0c89066af1630191b
idodd <idoddii@gmail.com>
2023-04-11 17:36:30 +0300

    logout api is done

--- Backend/PlantItApp/controllers/Accounts/urls.py
+++ Backend/PlantItApp/controllers/Accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import LoginController, RegisterController, PasswordController, UsernameController
+from . import LoginController, RegisterController, PasswordController, UsernameController, LogoutController
 
 
 urlpatterns = [
@@ -7,6 +7,7 @@ urlpatterns = [
     path('google-login/', LoginController.google_login),
     path('register/', RegisterController.register),
     path('google-register/', RegisterController.google_register),
+    path('logout/', LogoutController.logout),
     path('forgot-password/', PasswordController.forgot_password),
     path('set-username/', UsernameController.set_username),
 ]
