c7cf77566efa50e12d96252ded4f46eec69c23c6
Santiago Szterenberg <santiago.szterenberg@weka.io>
2023-04-12 19:06:23 +0300

    delete redundant codes

--- Frontend/lib/main.dart
+++ Frontend/lib/main.dart
@@ -1,25 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:frontend/constants.dart';
 import 'package:frontend/logins/loginScreen.dart';
 import 'package:frontend/service/googleSignIn.dart';
 import 'package:provider/provider.dart';
 import 'firebase_options.dart';
 
-final lightColorScheme = ColorScheme.light(
-  primary: const Color.fromARGB(255, 31, 85, 56),
-  secondary: Colors.black54,
-  // other colors
-);
-
-final darkColorScheme = ColorScheme.dark(
-  primary: const Color.fromARGB(255, 31, 85, 56),
-  secondary: Colors.white54,
-  // other colors
-);
-
-final lightTheme = ThemeData.from(colorScheme: lightColorScheme);
-final darkTheme = ThemeData.from(colorScheme: darkColorScheme);
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
