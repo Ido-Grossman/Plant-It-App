e57c7c14b6923d47b1623e344d465a2e7725e063
idodd <idoddii@gmail.com>
2023-04-15 14:15:14 +0300

    Added an api that returns a speicfic user details

--- Backend/PlantItApp/urls.py
+++ Backend/PlantItApp/urls.py
@@ -4,5 +4,6 @@ from .controllers import PhotoController
 
 urlpatterns = [
     path('accounts/', include('PlantItApp.controllers.Accounts.urls')),
+    path('users/', include('PlantItApp.controllers.Users.urls')),
     path('photo-upload/', PhotoController.PhotoUploadView.as_view(), name='photo_upload'),
 ]
