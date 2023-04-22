ca7c9d10fed33bfa6dbeee8d1f7f6a07501e7312
idodd <idoddii@gmail.com>
2023-04-22 20:23:56 +0300

    Search for plants is ready with filters if added

--- Backend/PlantItApp/controllers/Plants/urls.py
+++ Backend/PlantItApp/controllers/Plants/urls.py
@@ -4,5 +4,5 @@ from . import PlantController
 
 urlpatterns = [
     path('<int:plant_id>/', PlantController.get_plant_details, name='get_specific_plant'),
-    re_path(r'^(?P<plant_name>[\w\-]*)/$', PlantController.search_plants, name='get_plants')
+    re_path(r'^search/(?P<plant_name>[\w\-]*)/$', PlantController.search_plants, name='search_plants')
 ]
