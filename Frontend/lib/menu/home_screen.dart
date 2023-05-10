a1eb023782408551e1ef91cb47260c0fedf7663e
Santiago Szterenberg <santisz7897@gmail.com>
2023-05-10 22:40:38 +0300

    remove floating button

--- Frontend/lib/menu/home_screen.dart
+++ Frontend/lib/menu/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:device_calendar/device_calendar.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'dart:async';
